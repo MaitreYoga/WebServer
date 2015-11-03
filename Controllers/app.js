0efdad26d9a7f5d6cf68cee105b136b01f8428b4
Joris Maillet <jorismaillet@gmail.com>
2015-11-03 22:23:09 +0100

    Add current source code

---
+++ Controllers/app.js
@@ -0,0 +1,9 @@
+﻿var app = angular.module('ZenLounge', []);
+
+app.controller('UserController', function($http, $scope){
+	$scope.result = "1";
+	this.HelloWorld = function()
+	{
+		$http.get('http://localhost:8080/ZenLounge/HelloWorld').success(function(response){alert(response+"\n"+response.data+"\n"+response.status+"\n"+response.headers); $scope.result = response.data});
+	};
+});
