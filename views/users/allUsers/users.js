85576418bcf3304ca150c45758da706da8946abb
Joris Maillet <jorismaillet@gmail.com>
2015-11-09 15:40:47 +0100

    Add encryption algorithm

--- views/users/allUsers/users.js
+++ views/users/allUsers/users.js
@@ -2,5 +2,7 @@
  * Created by jorismaillet on 06/11/15.
  */
 angular.module('ZenLounge').controller('UsersController', ['$scope','webcallservice', function($scope, webcallservice){
-    $scope.getUsers = webcallservice.getUsers(function(data) {$scope.users=data;});
+    $scope.getUsers = webcallservice.getUsers(function(data) {
+		$scope.users=data;
+	});
 }]);
