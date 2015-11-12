daa6e74211f3002a971bcdf5da7de6d85978bff1
lucasesteban1er <lucas.estebanzucco@gmail.com>
2015-11-12 12:20:45 +0100

    pas fait exprès de faire sur master sorry

--- views/common/navbar/navbarCtrl.js
+++ views/common/navbar/navbarCtrl.js
@@ -1,21 +1,6 @@
 /**
  * Created by lucasesteban on 04/11/15.
  */
-angular.module('ZenLounge').controller('TestController', ['$scope','webcallservice','$cookies', function($scope, webcallservice,$cookies){
-    $scope.login = function(login,mdp) {
-        webcallservice.login(login, mdp,
-            function (response) {
-                $scope.test=response;
-                alert(response.data.firstName);
-                $cookies.put('userID',response.data.firstName);
-                $cookies.putObject('user',response.data);
-                alert($cookies.get('user'));
-                alert($cookies.get('userID'));
-
-            },
-            function (response) {
-                alert('connection failed' + response);
-                redirectTo('/')
-            });
-    }
+angular.module('ZenLounge').controller('TestController', ['$scope','webcallservice','$cookies','$rootScope', function($scope, webcallservice,$cookies,$root){
+    $root.user=$cookies.getObject('user');
 }]);
