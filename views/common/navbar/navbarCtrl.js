3117f9b23a56570a1ef12d97952abe55acf2ad16
Joris Maillet <jorismaillet@gmail.com>
2015-11-13 11:57:26 +0100

    Can close CookiesInfos bar

--- views/common/navbar/navbarCtrl.js
+++ views/common/navbar/navbarCtrl.js
@@ -1,6 +1,13 @@
 /**
  * Created by lucasesteban on 04/11/15.
  */
-angular.module('ZenLounge').controller('TestController', ['$scope','webcallservice','$cookies','$rootScope', function($scope, webcallservice,$cookies,$root){
+angular.module('ZenLounge').controller('NavbarController', ['$scope','webcallservice','$cookies','$rootScope', function($scope, webcallservice,$cookies,$root){
     $root.user=$cookies.getObject('user');
+	
+	$scope.cookiesInfos = ($cookies.get("cookiesInfos") === undefined);
+	
+	$scope.hideCookiesInfos = function(){
+		$scope.cookiesInfos = false;
+		$cookies.put("cookiesInfos",false);
+	};
 }]);
