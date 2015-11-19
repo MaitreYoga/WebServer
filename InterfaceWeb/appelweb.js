4344a63151f7941345050979c264863f55afd660
RemyTartiere <remy@MacBook-Pro-de-Matthieu.local>
2015-11-19 11:43:09 +0100

    adresse prod bis

--- InterfaceWeb/appelweb.js
+++ InterfaceWeb/appelweb.js
@@ -4,8 +4,9 @@
 angular.module('WebCall', []).
     service('webcallservice', ['$http', function ($http) {
         //api='https://zenlounge-restapi.herokuapp.com';
-        api = 'http://localhost:8080/ZenLounge';
-
+        //api = 'http://localhost:8080/ZenLounge';
+        api='https://zenlounge-maven.herokuapp.com';
+        
         this.login = function (login, mdp, success, error) {
             $http({
                 url: api + '/login',
