f99beaa8ae1375a5ab40b3d3aab5d26475c97eb3
RemyTartiere <remy.tartiere@free.fr>
2015-11-22 10:55:43 +0100

    erreur presentatioin code

--- views/shop/products/brands/productBrand.js
+++ views/shop/products/brands/productBrand.js
@@ -1,20 +1,20 @@
 /**
  * Created by remytartiere on 21/11/15.
  */
-angular.module('ZenLounge').controller('ProductBrandsController', ['$scope','webcallservice', function($scope, webcallservice){
-    $scope.getBrands = webcallservice.getBrands(function(data) {
-		$scope.productBrands=data.data;
+angular.module('ZenLounge').controller('ProductBrandsController', ['$scope', 'webcallservice', function ($scope, webcallservice) {
+    $scope.getBrands = webcallservice.getBrands(function (data) {
+		$scope.productBrands = data.data;
 	});
 
-	$scope.addProductBrand = function() {
-        $scope.brand.id="";
-        webcallservice.addProductBrands($scope.brand,function(data) {
+	$scope.addProductBrand = function () {
+        $scope.brand.id = "";
+        webcallservice.addProductBrands($scope.brand, function (data) {
             alert("ok");
 			$scope.brand.name = "";
 		})
 	};
 
-    $scope.delete= function(brand) {
+    $scope.delete = function (brand) {
         webcallservice.deleteProductBrands(brand.id, function () {alert('ok')});
     }
 }]);
